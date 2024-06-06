124b44d0da6870c4ebfa858b0a6aa89ca3a43fb6
Harry <s6968678@gmail.com>
2024-06-07 06:39:45 +0800

    Your new commit message

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [vue()],
+  base: "/farm-commerce/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
