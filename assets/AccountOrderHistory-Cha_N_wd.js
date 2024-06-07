a11eb30feb380cfc26c9df615cc41071d4b7e091
Harry <s6968678@gmail.com>
2024-06-08 05:50:08 +0800

    Updates

---
+++ assets/AccountOrderHistory-Cha_N_wd.js
@@ -0,0 +1,1 @@
+import{A as t}from"./AccountEmptyCard-zmp3HHmY.js";import{o as e,c as o,a as c}from"./index-DyLQbmbx.js";const r={class:"section-container"},p={__name:"AccountOrderHistory",setup(s){return(a,n)=>(e(),o("div",r,[c(t,{title:"以完成訂單",description:"目前沒有完成的訂單"})]))}};export{p as default};
